00990c9c445d4d5c41c9e461a17f699e05a0af2a
VTTcgn <tanthoivo857@gmail.com>
2025-03-13 15:22:10 +0700

    Update Bai1 vs Bai2

---
+++ Bai1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào bán kính r = ");
+        double r = sc.nextDouble();
+        int N = 2000000;
+        int M = 0;
+
+        Random random = new Random();
+
+        for (int i=0; i<N; i++) {
+            double x = 2*r*random.nextDouble() - r;
+            double y = 2*r*random.nextDouble() - r;
+
+            if ((x*x + y*y) <= r*r) M++;
+        }
+        double Dientich = (double) M / N * 4 * r * r;
+        System.out.println("Diện tích hình trong xấp xỉ là: " + Dientich);
+    }
+}
